7cf7e3298348f1ed722354153270ea74029dfc73
Cr1stopherB <cristopherbravo13@gmail.com>
2025-11-19 16:27:26 -0300

    Modificacion dde modelo USUARIO

--- src/main/java/com/example/demo/controller/HealthController.java
+++ src/main/java/com/example/demo/controller/HealthController.java
@@ -2,8 +2,10 @@ package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public class EstadoRenderController {
+@RestController
+public class HealthController {
     @GetMapping("/health")
     public ResponseEntity<String> health() {
         return ResponseEntity.ok("OK");
