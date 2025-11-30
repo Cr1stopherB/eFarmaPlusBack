5f2cef6baed27629b9f61df0fb98c76e03f5dd9a
Cr1stopherB <cristopherbravo13@gmail.com>
2025-11-30 19:13:03 -0300

    Agregando relacion onetomany con imagenes

--- src/main/java/com/example/demo/model/Producto.java
+++ src/main/java/com/example/demo/model/Producto.java
@@ -1,12 +1,18 @@
 package com.example.demo.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -49,4 +55,7 @@ public class Producto {
     @Column(name = "REQUIERE_RECETA", nullable = false)
     private Boolean requiereReceta;
 
+    @OneToMany(mappedBy = "producto", fetch = FetchType.EAGER)
+    @JsonIgnoreProperties("producto")
+    private List<Imagenes> imagenes;
 }
